19c93e6f46b5a8f9b3d4cd630abc3b910a8d9dd9
bnicol-ensc <bnicol@ensc.fr>
2019-03-05 13:41:12 +0100

    Update database.sql

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,4 @@
-create database if not exists mymovies character set utf8 collate utf8_unicode_ci;
+create database if not exists escape_the_void character set utf8 collate utf8_unicode_ci;
 use escape_the_void;
 
-grant all privileges on mymovies.* to 'escape_the_void_user'@'localhost' identified by 'secret';
+grant all privileges on escape_the_void.* to 'escape_the_void_user'@'localhost' identified by 'secret';
