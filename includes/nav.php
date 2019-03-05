65ec30a01a4c524fb4e47e96585b42d59866c95f
mahautm <45102506+mahautm@users.noreply.github.com>
2019-03-05 15:58:47 +0100

    nav moins moche

--- includes/nav.php
+++ includes/nav.php
@@ -1,28 +1,39 @@
-<nav class="navbar navbar-toggleable-sm navbar-trans navbar-inverse">
-				<div class="container">
-					<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler-1" aria-controls="navbarToggler-1" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<a class="navbar-brand" href="#">Bootstrap 4 Cover Nav</a>
-					<div class="collapse navbar-collapse pull-xs-right justify-content-end" id="navbarToggler-1">
-						<ul class="navbar-nav mt-2 mt-md-0">
-							<li class="nav-item active">
-								<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">About</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Our Work</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Contact</a>
-							</li>
-							<li class="nav-item">
-								<a class="btn btn-outline-white btn-outline" href="#"><i class="fa fa-user"></i> Login
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">
+      <img src="images/ApertureLogo2.jpg" width="30" height="30" class="d-inline-block align-top" alt="">  
+      Escape Game
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <?php if(isset($_SESSION['login'])) echo "<a class='nav-item nav-link' href='movie_add.php'>Add Movie</a>";?>
+        </li>
+      </ul>
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <img src="images/login.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            <?php 
+                if(isset($_SESSION['login'])) echo 'Bonjour, '.$_SESSION['login'];
+                else echo 'Non Connecté';
+            ?>
+        </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href=
+          <?php 
+            if(isset($_SESSION['login'])) echo "'logout.php'";
+            else echo "'login.php'";?>>
+          <?php 
+            if(isset($_SESSION['login'])) echo 'Se Déconnecter';
+            else echo 'Se Connecter';
+          ?>
+        </a>
+  </div>
+</div>
+    </div>
+  </div>
+</nav>
