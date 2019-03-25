5f2fc1675e68e302d0820121132994fa90e4addc
Mahaut <mmahaut@ensc.fr>
2019-03-25 11:35:19 +0100

    Unworking tests on JOUER.php

--- includes/head.php
+++ includes/head.php
@@ -2,8 +2,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
     <link href="lib/fontawesome/css/all.css" rel="stylesheet">
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
     <title>Escape Game <?php if (isset($pageTitle)) echo " - $pageTitle"; ?></title>
 </head>
--- jouer.php
+++ jouer.php
@@ -16,7 +16,24 @@
                 require_once("includes/nav.php");
                 require_once("includes/connect.php");
         ?>
-            
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-7 d-flex">
+                    <div class="console">
+                        >>This is a test console message
+                    </div>
+                </div>
+                <div class="col-md-5 d-flex">
+                    <input type="checkbox" disabled data-toggle="toggle" data-onstyle="warning">
+                    <input type="checkbox" checked data-toggle="toggle" data-onstyle="warning">
+                    <input type="checkbox" checked data-toggle="toggle" data-onstyle="warning">
+                    <input type="checkbox" checked data-toggle="toggle" data-onstyle="danger">
+                </div>
+        </div>
+        <div class="row">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 40%">
+        </div>
+        </div>
         <?php require_once("includes/footer.php");?>
         <?php require_once("includes/script.php"); ?>
     </div>
