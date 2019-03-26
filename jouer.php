852400dc9f1bc8ac1a627eb61d4e6935514bb79b
mahautm <45102506+mahautm@users.noreply.github.com>
2019-03-26 15:58:05 +0100

    Not working scrollText
    A lot of not working stuff

--- jouer.php
+++ jouer.php
@@ -19,37 +19,33 @@
             <div class="row">
                 <div class="col-md-6 d-flex">
                     <div class="console w-100 mh-100 rounded">
-                        Ship >> Loading Emergency Module ... </br>
-                        Ship >> Do not Panic.
+                        <div id="typedtext"></div>
                     </div>
                 </div>
                 <div class="col-md-3 console-buttons">
                     <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown">
+                    <div class="btn-group" role="group">
+                            <div class="col-auto my-1">
+                                <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                    <option selected disabled>-- Alimentation --</option>
+                                    <option value="1">Générateur Principal</option>
+                                    <option value="2" >Générateur d'urgence</option>
+                                    <option value="3" disabled>Générateur Secondaire</option>
+                                </select>
+                            </div>
+                        </div>
                         <button type="button" class="btn btn-console rounded">Lumière</button>
                         <button type="button" class="btn btn-console rounded">Navette Secours</button>                        <button type="button" class="btn btn-console rounded">Navette Secours</button>
                         <button type="button" class="btn btn-console rounded">Diagnostique</button>                        <button type="button" class="btn btn-console rounded">Navette Secours</button>
                         <button type="button" class="btn btn-console rounded disabled">Réparations</button>
                         <button type="button" class="btn btn-console rounded disabled">Communication</button>
-                        <div class="btn-group" role="group">
-                            <div class="col-auto my-1">
-                                <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
-                                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                    <option selected disabled>Alimentation</option>
-                                    <option value="1">Générateur Principal</option>
-                                    <option value="2">Générateur Secondaire</option>
-                                    <option value="3">Générateur d'urgence</option>
-                                </select>
-                            </div>
-                        </div>
                     </div>
                 </div>
 
                 <div class="col-md-3 d-flex">
                     <div class="progress">
-                        <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                        test    
-                    
-                        </div>
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 40%"></div>
                     </div>
                 </div>
             </div>
@@ -63,6 +59,13 @@
 
         </div>
     </div>
+        <script src="includes/typewriter.js"></script>
+        <script>
+            typewriter(initAText(
+                "AAAAAAA"
+            ));
+        </script>
+
         <?php require_once("includes/footer.php");?>
         <?php require_once("includes/script.php"); ?>
     
