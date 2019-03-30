6a145ca4d03ac9c92b7a1d602239a4124944be2d
Mahaut <mmahaut@ensc.fr>
2019-03-30 12:32:28 +0100

    inclure le chat n'importe comment dans la page jouer

--- jouer.php
+++ jouer.php
@@ -22,6 +22,7 @@ $_SESSION['eng_id'] = 1;
             <?php       
                     require_once("includes/nav.php");
                     require_once("includes/connect.php");
+                    
 
                     if($BDD){
                         $MaRequete = "SELECT eng_content FROM enigme WHERE eng_id=".$_SESSION['eng_id'];
@@ -40,6 +41,7 @@ $_SESSION['eng_id'] = 1;
 
                     }
             ?>
+            
             <div class="row">
                 <div class="col-md-6 d-flex">
                     <div class="console w-100 mh-100 rounded">
@@ -47,28 +49,48 @@ $_SESSION['eng_id'] = 1;
                     </div>
                 </div>
                 <div class="col-md-3 console-buttons">
-                    <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown">
-                    <div class="btn-group" role="group">
-                            <div class="col-auto my-1">
-                                <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
-                                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                    <option selected disabled>  --- Alimentation ---  </option>
-                                    <option value="1">Générateur Principal</option>
-                                    <option value="2" >Générateur d'urgence</option>
-                                    <option value="3" disabled>Générateur Secondaire</option>
-                                </select>
+                    <form action="action.php" method="post">
+                        <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown">
+                        <div class="btn-group" role="group">
+                                <div class="col-auto my-1">
+                                    <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                        <option selected disabled>  --- Alimentation ---  </option>
+                                        <option value="1">Générateur Principal</option>
+                                        <option value="2" >Générateur d'urgence</option>
+                                        <option value="3" disabled>Générateur Secondaire</option>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
-                        <button type="button" class="btn btn-console rounded">Lumière</button>
-                        <button type="button" class="btn btn-console rounded">Navette Secours</button>
-                        <button type="button" class="btn btn-console rounded">Diagnostique</button>
-                        <button type="button" class="btn btn-console rounded disabled">Réparations</button>
-                        <button type="button" class="btn btn-console rounded disabled">Communication</button>
+                            <label class="switch">
+	                            <input class="switch-input" type="checkbox" />
+	                            <span class="switch-label" data-on="On" data-off="Off"></span> 
+	                            <span class="switch-handle"></span> 
+                            </label>
+                            <button type="button" class="btn btn-console rounded">Lumière</button>
+                            <button type="button" class="btn btn-console rounded">Navette Secours</button>
+                            <button type="button" class="btn btn-console rounded">Diagnostique</button>
+                            <button type="button" class="btn btn-console rounded disabled">Réparations</button>
+                            <button type="button" class="btn btn-console rounded disabled">Communication</button>
+                    </form>
                     </div>
                 </div>
 
                 <div class="col-md-3 d-flex">
-
+                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#chatbox" aria-expanded="false" aria-controls="chatbox">
+                    Ouvrir le chat
+                    </button>
+                    <div class="collapse" id="chatbox">
+                        <div class="container-fluid row h-100">
+                            <div id="page-wrap">
+                                <h2>Chat</h2>
+                                <div id="chat-wrap"><div id="chat-area"></div></div>
+                                <form id="send-message-area">
+                                    <p>Saisir votre message : </p>
+                                    <textarea id="sendie" maxlength = '100' ></textarea>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
