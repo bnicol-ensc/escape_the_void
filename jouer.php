7c3e016606adb7d331cdbbace6a70003f9193ed7
Mahaut <mmahaut@ensc.fr>
2019-03-30 21:03:09 +0100

    push car j'ai du texte qui s'affiche à l'écran
    Petite victoire personnelle

--- jouer.php
+++ jouer.php
@@ -13,7 +13,120 @@
 <?php
 $_SESSION['eng_id'] = 1;
 ?>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+     <div class="background">
+        <div class="container-fluid">
+            <?php       
+                    require_once("includes/nav.php");
+                    require_once("includes/connect.php");
+                    
+
+                    if($BDD){
+                        $MaRequete = "SELECT * FROM bouton WHERE eng_id=".$_SESSION['eng_id'];
+        
+        
+                        $STH = $BDD -> prepare( $MaRequete );
+        
+                        $STH -> execute();
+        
+                        $result = $STH -> fetch();
+
+                    }
+            ?>
+            
+            <div class="row">
+                <div class="col-md-3 console-buttons">
+                    <form action="action.php" method="post">
+                        <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown">
+                        <div class="btn-group" role="group">
+                                <div class="col-auto my-1">
+                                    <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                        <option selected disabled>  --- Alimentation ---  </option>
+                                        <option value="1">Générateur Principal</option>
+                                        <option value="2" >Générateur d'urgence</option>
+                                        <option value="3" disabled>Générateur Secondaire</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <?php 
+                            for ($i = 0; $i<6 ;$i++) {
+                                echo "<label class=\"switch\">";
+                                //echo $result['btn_hidden']."
+                                echo "<input class=\"switch-input\" type=\"checkbox\" />";
+                                echo "<span class=\"switch-label\" data-on=\"".$result['btn_name']."\" data-off=\"Lumière\"></span> ";
+                                echo "<span class=\"switch-handle\"></span></label>";
+                            }
+                            
+                            ?>
+                            <button type="button" class="btn btn-console rounded">Lumière</button>
+                            <button type="button" class="btn btn-console rounded">Navette Secours</button>
+                            <button type="button" class="btn btn-console rounded">Diagnostique</button>
+                            <button type="button" class="btn btn-console rounded disabled">Réparations</button>
+                            <button type="button" class="btn btn-console rounded disabled">Communication</button>
+                    </form>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="console w-100 mh-100 rounded">
+                        <div id="typedtext"></div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#chatbox" aria-expanded="false" aria-controls="chatbox">
+                    Demander de l'aide
+                    </button>
+                    <div class="collapse" id="chatbox">
+                        <div class="container-fluid row h-100">
+                            <div id="page-wrap">
+                                <h2>Chat</h2>
+                                <div id="chat-wrap"><div id="chat-area"></div></div>
+                                <form id="send-message-area">
+                                    <p>Saisir votre message : </p>
+                                    <textarea id="sendie" maxlength = '100' ></textarea>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-6 d-flex">
+
+                </div>
+                <div class="col-md-3 d-flex">
+                </div>
+            </div>
+
+        </div>
+    </div>
+        <script>
+
+            var aText = new Array(
+            <?php 
+                echo "\"";
+
+                foreach($result as $row) {
+                    echo $row;
+                }
+
+                echo "\""; 
+            ?>
+
+            );
+        </script>
+        <script src="includes/typewriter.js"></script>
+        <script src="includes/flash.js"></script>
+
+        <script>
+        //initialiseText();
+        typewriter();
+        lightning();
+        </script>
+
+        <?php require_once("includes/footer.php");?>
+        <?php require_once("includes/script.php"); ?>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script type="text/javascript" src="./includes/chat.js"></script>
 <script type="text/javascript">
 
@@ -70,121 +183,7 @@ $_SESSION['eng_id'] = 1;
         
     });
 </script>
-     <div class="background">
-        <div class="container-fluid">
-            <?php       
-                    require_once("includes/nav.php");
-                    require_once("includes/connect.php");
-                    
 
-                    if($BDD){
-                        $MaRequete = "SELECT eng_content FROM enigme WHERE eng_id=".$_SESSION['eng_id'];
-        
-        
-                        $STH = $BDD -> prepare( $MaRequete );
-        
-                        $STH -> execute();
-        
-                        $result = $STH -> fetch();
-
-                        //partie JS : Si un des boutons a été pressé et qu'il gère le javascript, alors l'activer.
-                        for($i = 0 ; $i < 6 ; $i++) {
-                            //if($result['btn'.$_SESSION['eng_id'].'_type']);
-                        }
-
-                    }
-            ?>
-            
-            <div class="row">
-                <div class="col-md-3 console-buttons">
-                    <form action="action.php" method="post">
-                        <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown">
-                        <div class="btn-group" role="group">
-                                <div class="col-auto my-1">
-                                    <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
-                                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                        <option selected disabled>  --- Alimentation ---  </option>
-                                        <option value="1">Générateur Principal</option>
-                                        <option value="2" >Générateur d'urgence</option>
-                                        <option value="3" disabled>Générateur Secondaire</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <?php 
-                            for ($i = 0; $i<6;$i++) {
-                                echo "<label class=\"switch\">";
-                                echo "<input class=\"switch-input\" type=\"checkbox\" />";
-                                echo "<span class=\"switch-label\" data-on=\"Lumière\" data-off=\"Lumière\"></span> ";
-                                echo "<span class=\"switch-handle\"></span></label>";
-                            }
-                            
-                            ?>
-                            <button type="button" class="btn btn-console rounded m-*-auto">Lumière</button>
-                            <button type="button" class="btn btn-console rounded">Navette Secours</button>
-                            <button type="button" class="btn btn-console rounded">Diagnostique</button>
-                            <button type="button" class="btn btn-console rounded disabled">Réparations</button>
-                            <button type="button" class="btn btn-console rounded disabled">Communication</button>
-                    </form>
-                    </div>
-                </div>
-                <div class="col-md-6 d-flex">
-                    <div class="console w-100 mh-100 rounded">
-                        <div id="typedtext"></div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#chatbox" aria-expanded="false" aria-controls="chatbox">
-                    Demander de l'aide
-                    </button>
-                    <div class="collapse" id="chatbox">
-                        <div class="container-fluid row h-100">
-                            <div id="page-wrap">
-                                <h2>Chat</h2>
-                                <div id="chat-wrap"><div id="chat-area"></div></div>
-                                <form id="send-message-area">
-                                    <p>Saisir votre message : </p>
-                                    <textarea id="sendie" maxlength = '100' ></textarea>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-md-6 d-flex">
-
-                </div>
-                <div class="col-md-3 d-flex">
-                </div>
-            </div>
-
-        </div>
-    </div>
-        <script>
-
-            var aText = new Array(
-            <?php 
-                echo "\"";
-
-                echo $result["eng_content"];
-
-                echo "\""; 
-            ?>
-
-            );
-        </script>
-        <script src="includes/typewriter.js"></script>
-        <script src="includes/flash.js"></script>
-
-        <script>
-        //initialiseText();
-        typewriter();
-        lightning();
-        </script>
-
-        <?php require_once("includes/footer.php");?>
-        <?php require_once("includes/script.php"); ?>
     
 </body>
 
